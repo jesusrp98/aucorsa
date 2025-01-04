f5f82782caa744c994839d3cd9908084511791cb
jesusrp98 <jesusrope98@outlook.com>
2025-01-04 12:40:21 +0100

    Add brand assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:aucorsa/app.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
@@ -8,9 +7,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory: kIsWeb
-        ? HydratedStorage.webStorageDirectory
-        : await getApplicationDocumentsDirectory(),
+    storageDirectory: await getApplicationDocumentsDirectory(),
   );
 
   return runApp(const AucorsaApp());
